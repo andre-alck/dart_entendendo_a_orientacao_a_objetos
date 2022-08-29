aaf956d36dbeed9df8264682891287b604301fe0
andre-alck <andrealck1@gmail.com>
2022-08-29 11:38:31 -0300

    first commit

---
+++ bin/dart_entendendo_a_orientacao_a_objeto.dart
@@ -0,0 +1,5 @@
+import 'package:dart_entendendo_a_orientacao_a_objeto/dart_entendendo_a_orientacao_a_objeto.dart' as dart_entendendo_a_orientacao_a_objeto;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_entendendo_a_orientacao_a_objeto.calculate()}!');
+}
