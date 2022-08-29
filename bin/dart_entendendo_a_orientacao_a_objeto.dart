8f6350a67ebea79bb546088faa52f6a0582639b8
andre-alck <andrealck1@gmail.com>
2022-08-29 13:22:58 -0300

    add cap2

--- bin/dart_entendendo_a_orientacao_a_objeto.dart
+++ bin/dart_entendendo_a_orientacao_a_objeto.dart
@@ -1,5 +1,40 @@
-import 'package:dart_entendendo_a_orientacao_a_objeto/dart_entendendo_a_orientacao_a_objeto.dart' as dart_entendendo_a_orientacao_a_objeto;
+void main() {
+  String nome = 'laranja';
+  double peso = 100.2;
+  String corDaFruta = 'Verde e Amarela';
+  String sabor = 'Doce e cítrica';
+  int diasDesdeAColheita = 20;
+  bool isMadura;
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_entendendo_a_orientacao_a_objeto.calculate()}!');
+  isMadura = funcEstaMadura(diasDesdeAColheita);
+  print(isMadura);
+
+  mostrarMadura(30, nome: nome);
+  mostrarMadura(30, nome: nome, corDaFruta: corDaFruta);
+  mostrarMadura(30, nome: nome, corDaFruta: corDaFruta, sabor: "Ok.");
+
+  int diasAteAmadurecer = funcDiasAteAmadurecer(diasDesdeAColheita);
+  print(diasAteAmadurecer);
+}
+
+bool funcEstaMadura(int diasDesdeAColheita) {
+  return diasDesdeAColheita >= 30;
+}
+
+void mostrarMadura(int diasDesdeAColheita, {String? corDaFruta, String sabor = "Ok", required String nome}) {
+  if (diasDesdeAColheita >= 30) {
+    if (corDaFruta != null) {
+      print('A $nome está madura. Sua cor é $corDaFruta. Seu sabor é $sabor.');
+    } else {
+      print('A $nome está madura. Seu sabor é $sabor.');
+    }
+  } else {
+    print('A $nome não está madura. Seu sabor é $sabor.');
+  }
+}
+
+funcDiasAteAmadurecer(int diasDesdeAColheita) {
+  int totalDeDiasParaAmadurecer = 30;
+  int quantosDiasFaltam = totalDeDiasParaAmadurecer - diasDesdeAColheita;
+  return quantosDiasFaltam;
 }
