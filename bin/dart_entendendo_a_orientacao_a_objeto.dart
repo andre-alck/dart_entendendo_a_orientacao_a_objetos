244b1930a05ab5ee3fbfbcdcb3fbf3400eb73dec
andre-alck <andrealck1@gmail.com>
2022-08-29 17:07:52 -0300

    add cap4

--- bin/dart_entendendo_a_orientacao_a_objeto.dart
+++ bin/dart_entendendo_a_orientacao_a_objeto.dart
@@ -1,54 +1,88 @@
-void main() {
-  Fruta fruta01 = Fruta("Banana", 100, 'Amarela', 'Doce', 15);
-  Fruta fruta02 = Fruta("Laranja", 75, 'Laranja', 'Azeda', 30);
-  Fruta fruta03 = Fruta.nomeados(
-      nome: 'Limão',
-      peso: 50,
-      corDaFruta: 'Verde',
-      sabor: 'Azeda',
-      diasDesdeAColheita: 60);
-  Fruta fruta04 = Fruta.minusculas("Ameixa", 50, 'Vinho', 'Doce', 15);
+main() {
+  Alimento arroz = Alimento("Arroz", 1.0, "Branco");
+  Fruta banana = Fruta('Banana', 50.0, 'Amarela', 'Doce', 7);
+  Legume chuchu = Legume('Chuchu', 100.0, 'Verde', true);
+  Citricas limao = Citricas('Limão', 15, 'Verde', 'Azedo', 30, 100);
+  Nozes macadamia = Nozes('Macadâmia', 5, 'Marrom', 'Amargo', 50, 50);
 
-  print(fruta01.nome);
-  print(fruta02.nome);
-  print(fruta03.nome);
-  print(fruta04.nome);
+  arroz.mostraAlimento();
+  banana.mostraAlimento();
+  chuchu.mostraAlimento();
+  limao.mostraAlimento();
+  macadamia.mostraAlimento();
 
-  print(fruta01.estaMadura(10));
-  print(fruta02.estaMadura(20));
-  print(fruta03.estaMadura(30));
-  print(fruta04.estaMadura(40));
+  banana.fazerSuco();
+  chuchu.cozinhar();
+  limao.provar();
+  macadamia.quebrar();
 }
 
-class Fruta {
+class Alimento {
   String nome;
   double peso;
-  String corDaFruta;
+  String cor;
+
+  Alimento(this.nome, this.peso, this.cor);
+
+  void mostraAlimento() {
+    print('Este(a) $nome pesa $peso gramas. Sua cor é $cor.');
+  }
+}
+
+class Fruta extends Alimento {
   String sabor;
   int diasDesdeAColheita;
   bool? isMadura;
 
-  Fruta(this.nome, this.peso, this.corDaFruta, this.sabor,
-      this.diasDesdeAColheita,
-      {this.isMadura});
-
-  Fruta.nomeados(
-      {required this.nome,
-      required this.peso,
-      required this.corDaFruta,
-      required this.sabor,
-      required this.diasDesdeAColheita,
-      this.isMadura});
-
-  Fruta.minusculas(this.nome, this.peso, this.corDaFruta, this.sabor,
-      this.diasDesdeAColheita,
-      {this.isMadura}) {
-    nome = nome.toLowerCase();
-    corDaFruta = corDaFruta.toLowerCase();
-    sabor = sabor.toLowerCase();
-  }
+  Fruta(
+      String nome, double peso, String cor, this.sabor, this.diasDesdeAColheita,
+      {this.isMadura})
+      : super(nome, peso, cor);
 
   String estaMadura(int diasAteAmadurecer) {
     return 'Sua $nome está madura? ${diasDesdeAColheita >= diasAteAmadurecer}';
+  }
+
+  void fazerSuco() {
+    print('Suco de $nome!');
+  }
+}
+
+class Legume extends Alimento {
+  bool isPrecisaCozinhar;
+
+  Legume(String nome, double peso, String cor, this.isPrecisaCozinhar)
+      : super(nome, peso, cor);
+
+  void cozinhar() {
+    if (isPrecisaCozinhar) {
+      print('Cozinhando... pronto!');
+    } else {
+      print('Este legume não precisa ser cozido.');
+    }
+  }
+}
+
+class Citricas extends Fruta {
+  double nivelDeAzedo;
+
+  Citricas(String nome, double peso, String cor, String sabor,
+      int diasDesdeAColheita, this.nivelDeAzedo)
+      : super(nome, peso, cor, sabor, diasDesdeAColheita);
+
+  void provar() {
+    print('Azedo...');
+  }
+}
+
+class Nozes extends Fruta {
+  double porcentagemDeOleoNatural;
+
+  Nozes(String nome, double peso, String cor, String sabor,
+      int diasDesdeAColheita, this.porcentagemDeOleoNatural)
+      : super(nome, peso, cor, sabor, diasDesdeAColheita);
+
+  void quebrar() {
+    print('Muito resistente.');
   }
 }
